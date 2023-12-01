e9ffc2fd3804f382a61e89c2246e0f8dcb6823ae
Evgeniy-Popov-98 <evgeniy6183@gmail.com>
2023-12-01 19:45:33 +0200

    HomeWork#4

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,24 @@
 'use strict';
 
-function getElementWidth(content, padding, border) {
-  content = Number.parseInt(content);
-  padding = Number.parseInt(padding) * 2;
-  border = Number.parseFloat(border) * 2;
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
 
-  const widht = content + padding + border;
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-  return widht;
-}
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
